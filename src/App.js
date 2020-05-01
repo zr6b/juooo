ec0391b639cd03f2049bb732f49e8359183ffcdd
hlqbaba <1446035617@qq.com>
2020-05-02 00:23:45 +0800

    frist

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import{
+    NavLink,
+} from"react-router-dom"
+import router from "./router/index"
+import MyRouter from "./components/common/MyRouter";
+
+function App() {
+  return (
+    <div className="App">
+        <MyRouter router={router}></MyRouter>
+
+      <nav>
+          <NavLink to={"/"} exact>首页</NavLink>|
+          <NavLink to={"/theatre"}>剧院</NavLink>|
+          <NavLink to={"/eticket"}>票夹</NavLink>|
+          <NavLink to={"/myjuooo"}>我的</NavLink>
+      </nav>
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
