69446c145c3f1224b666a9ecb7642810cf448da2
zhourui <410843653@qq.com>
2020-05-06 22:43:27 +0800

    11

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './App.css';
+import{
+    NavLink,
+} from"react-router-dom"
 import router from "./router/index"
 import MyRouter from "./components/common/MyRouter";
-import Table from "./components/common/Table"
+
 function App() {
   return (
     <div className="App">
-      <MyRouter router={router}></MyRouter>
-      <Table></Table>
+        <MyRouter router={router}></MyRouter>
+
+      <nav>
+          <NavLink to={"/"} exact>首页</NavLink>|
+          <NavLink to={"/theatre"}>剧院</NavLink>|
+          <NavLink to={"/eticket"}>票夹</NavLink>|
+          <NavLink to={"/myjuooo"}>我的</NavLink>
+      </nav>
+
+
+
+
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,18 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import axios from "axios"
-import './assets/rem'
-import {
-    Provider
-} from "react-redux";
-import store from "./store"
+import "lib-flexible";
 import * as serviceWorker from './serviceWorker';
 import{
     BrowserRouter as Router
 } from"react-router-dom"
 
 axios.interceptors.request.use(config=>{
-    config.url = "/api"+config.url;
+    config.url = "/dl"+config.url;
+    // config.url = "/api"+config.url;
     return config;
 })
 axios.interceptors.response.use(({data})=>{
@@ -24,11 +21,9 @@ axios.interceptors.response.use(({data})=>{
 React.Component.prototype.$axios = axios;
 ReactDOM.render(
   <React.StrictMode>
-      <Provider store={store}>
-          <Router>
-              <App />
-          </Router>
-      </Provider>
+      <Router>
+          <App />
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
